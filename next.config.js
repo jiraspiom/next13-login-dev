c83d329ea4222bc6b60d603b2c09c64a7b053906
1gilmar <1gilmar@gmail.com>
2024-02-28 16:28:32 -0300

    env no next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXTAUTH_URL: 'http://localhost:3000',
+    NEXTAUTH_SECRET: 'd41d8cd98f00b204e9800998ecf8427e',
+  },
+}
 
 module.exports = nextConfig
